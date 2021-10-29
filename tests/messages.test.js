5e6d902812918719a64e472cd6cd56c68600f43e
Seth McCullough <seth@endevrs.dev>
2021-10-29 12:56:40 -0400

    Add POST test

--- tests/messages.test.js
+++ tests/messages.test.js
@@ -6,4 +6,12 @@ describe("Test the root path", () => {
     const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
   });
+
+  test("Should set a message with the POST method", async () => {
+    const response = await request(app)
+      .post("/")
+      .send({ message: "Test message" });
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toEqual({ message: "Test message" });
+  });
 });
