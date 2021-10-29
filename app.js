20b816dda02c9c1dcd7bf37d4e40bba257467452
Seth McCullough <seth@endevrs.dev>
2021-10-29 12:35:09 -0400

    Separate app from server and install supertest

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import fs from "fs";
 import express from "express";
 const app = express();
-const PORT = 3000;
 
 app.use(express.json());
 
@@ -42,6 +41,4 @@ app.delete("/", (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
