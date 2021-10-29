97e7287d8b12670ae05e6afe391740a61f032b47
Seth McCullough <seth@endevrs.dev>
2021-10-29 07:45:57 -0400

    Add GET route

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Hello there! POST to this route to add another message!",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
