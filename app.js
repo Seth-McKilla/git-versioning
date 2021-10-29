aa6ea6ba89e65f7bc724de8eac9fd688fd5122b4
Seth McCullough <seth@endevrs.dev>
2021-10-29 12:56:40 -0400

    Add POST test

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.post("/", (req, res) => {
   const message = req.body;
   try {
     fs.writeFileSync("./db.json", JSON.stringify(message));
-    return res.send("Message successfully added!");
+    return res.status(201).send(message);
   } catch (error) {
     console.log(error);
     return res.status(500).send("Message failed to be added!");
